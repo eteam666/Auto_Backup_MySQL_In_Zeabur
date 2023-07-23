2509f614faaccfb76fdfe4ab648c2fcb13f7c67f
eteam666 <122675543+eteam666@users.noreply.github.com>
2023-07-23 18:52:08 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
 import os
-import subprocess
+import mysql.connector
 import boto3
-from time import sleep
-from datetime import datetime 
+from datetime import datetime
 
-seconds = os.getenv("TIME")
-seconds = int(seconds)
+# MySQL数据库连接信息
+mysql_host = os.getenv("MYSQL_HOST")
+mysql_port = os.getenv("MYSQL_PORT")
+mysql_username = os.getenv("MYSQL_USERNAME")
+mysql_password = os.getenv("MYSQL_PASSWORD")
+mysql_database = os.getenv("MYSQL_DATABASE")
+
+
+# AWS S3配置信息
+aws_region = os.getenv("AWS_REGION")
+s3_bucket = os.getenv("S3_BUCKET_NAME")
+aws_access_key = os.getenv("AWS_ACCESS_KEY_ID")
+aws_secret_key = os.getenv("AWS_SECRET_ACCESS_KEY")
+s3_endpoint = os.getenv("S3_ENDPOINT")
 
 def get_current_time():
     current_time = datetime.now()
@@ -13,47 +24,68 @@ def get_current_time():
     return formatted_time
 
 def backup_and_upload_to_s3():
-    mysql_host = os.getenv("MYSQL_HOST")
-    mysql_port = os.getenv("MYSQL_PORT")
-    mysql_username = os.getenv("MYSQL_USERNAME")
-    mysql_password = os.getenv("MYSQL_PASSWORD")
-    mysql_database = os.getenv("MYSQL_DATABASE")
-    aws_region = os.getenv("AWS_REGION")
-    s3_bucket = os.getenv("S3_BUCKET_NAME")
-    s3_object_key = "backup.sql"
-    aws_access_key = os.getenv("AWS_ACCESS_KEY_ID")
-    aws_secret_key = os.getenv("AWS_SECRET_ACCESS_KEY")
-    s3_endpoint = os.getenv("S3_ENDPOINT")
-
-    time_with_underscore = get_current_time()
-    backup_filename = f"{time_with_underscore}_backup.sql"
-
     try:
-        # Backup database to a local file
-        cmd = f"mysqldump -h {mysql_host} -P {mysql_port} -u {mysql_username} -p{mysql_password} {mysql_database} > {backup_filename}"
-        subprocess.run(cmd, shell=True, check=True)
+        # 连接到MySQL数据库
+        conn = mysql.connector.connect(
+            host=mysql_host,
+            port=mysql_port,
+            user=mysql_username,
+            password=mysql_password,
+            database=mysql_database
+        )
 
-        # Upload the backup file to S3
+        # 创建MySQL游标对象
+        cursor = conn.cursor()
+
+        # 获取数据库中所有表名
+        cursor.execute("SHOW TABLES;")
+        tables = [table[0] for table in cursor]
+        time_with_underscore = get_current_time()
+        backup_filename = f"{time_with_underscore}_backup.sql"
+        # 执行查询，将结果写入备份文件
+        with open(backup_file, 'w') as f:
+            for table in tables:
+                cursor.execute(f"SELECT * FROM {table};")
+                rows = cursor.fetchall()
+
+                # 写入表名和数据
+                f.write(f"TABLE: {table}\n")
+                for row in rows:
+                    row_str = ', '.join(str(col) for col in row)
+                    f.write(f"{row_str}\n")
+                f.write("\n")
+
+        print("数据库备份成功！")
+
+        # 上传备份文件到S3
         s3_client = boto3.client('s3', region_name=aws_region,
                                  aws_access_key_id=aws_access_key,
                                  aws_secret_access_key=aws_secret_key,
                                  endpoint_url=s3_endpoint)
-        with open(backup_filename, 'rb') as file:
+
+        time_with_underscore = get_current_time()
+        s3_object_key = f"{time_with_underscore}_backup.sql"
+
+        with open(backup_file, 'rb') as file:
             s3_client.upload_fileobj(file, s3_bucket, s3_object_key)
+
+        print("备份文件上传到S3成功！")
     except Exception as e:
-        print(f"An error occurred: {e}")
+        print(f"出现错误：{e}")
+    finally:
+        # 关闭数据库连接
+        if conn.is_connected():
+            cursor.close()
+            conn.close()
 
 if __name__ == "__main__":
     try:
-        cmd = ("sudo apt-get update")
-        subprocess.run(cmd, shell=True, check=True)
-        cmd = ("sudo apt-get install mysql-client")
-        subprocess.run(cmd, shell=True, check=True)
         while True:
             print("开始上传")
-            sleep(seconds)
+            sleep(3600)  # 备份间隔时间，单位为秒
             backup_and_upload_to_s3()
             print("上传结束")
     except KeyboardInterrupt:
         print("脚本已手动停止")
+
 
